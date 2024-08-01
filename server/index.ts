afa1eb96920152a4da867eacb1557786386df1a6
Антон Федулов <fedulov.anton@outlook.com>
2024-08-01 09:31:40 +0300

    save

--- server/index.ts
+++ server/index.ts
@@ -13,7 +13,7 @@ const startServer = async () => {
 
     serve({
         fetch: hanoRouter.fetch,
-        port: 4000,
+        port: 3000,
     });
 
     console.log('Server is running on port 3000');
