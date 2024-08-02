8762ebbf7dda27ffe1f036ae0db9d5bc34227dd1
Антон Федулов <fedulov.anton@outlook.com>
2024-08-02 09:31:43 +0300

    save

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,15 +1,17 @@
 import './Main.scss';
 import { useNavigate } from "react-router-dom";
+import * as rdd from 'react-device-detect';
 
 function Main() {
   const navigator = useNavigate();
+  const classDevice = rdd.isMobile ? 'is-phone' : 'is-pc';
   
   return (
-    <div className='main-page'>
+    <div className={'main-page ' + classDevice}>
       <header className="header">
         <img className="header-img" src={process.env.PUBLIC_URL + '/main/air_force.png'} alt='повітряні сили'></img>
         <img className="header-img book" src={process.env.PUBLIC_URL + '/main/book.jpg'} alt='книга памяті' onClick={() => navigator('/heroes')}></img>
-        <img className="header-img" src={process.env.PUBLIC_URL + '/main/griffin.png'} alt='бригада' onClick={() => navigator('/admin')}></img>
+        <img className="header-img" src={process.env.PUBLIC_URL + '/main/griffin.png'} alt='бригада' onClick={() => !rdd.isMobile && navigator('/admin')}></img>
       </header>
       <div className="description" >військовослужбовців 204 Севастопольської бригади тактичної авіації  які віддали своє життя за ради незалежності та територіальної цілісності держави</div>
     </div>
